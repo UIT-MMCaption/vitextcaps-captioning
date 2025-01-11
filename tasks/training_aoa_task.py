b30f52e63e1f5d31192a42aa4499f8f20b09d59c
DucAnh0Cap <danhngn1403@gmail.com>
2025-01-11 14:34:06 +0700

    Add train aoa task

--- tasks/training_aoa_task.py
+++ tasks/training_aoa_task.py
@@ -24,11 +24,10 @@ logger = setup_logger()
 
 
 class LanguageModelCriterion(nn.Module):
-
     def __init__(self):
         super(LanguageModelCriterion, self).__init__()
-        self.loss_fn = nn.NLLLoss(reduce='none')
-
+        self.loss_fn = nn.CrossEntropyLoss(reduction='none')
+        
     def forward(self, logits, target, mask):
         """
         logits: shape of (N, seq_len, vocab_size)
@@ -38,95 +37,27 @@ class LanguageModelCriterion(nn.Module):
         # truncate to the same size
         target = target[:, :logits.shape[1]]
         mask = mask[:, :logits.shape[1]]
+        
         logits = logits.contiguous().view(-1, logits.shape[2])
         target = target.contiguous().view(-1)
         mask = mask.contiguous().view(-1)
+        
         loss = self.loss_fn(logits, target)
         masked_loss = loss * mask
         output = masked_loss.sum() / mask.sum()  # Average over actual tokens
         return output
 
-class ContrastiveLoss(nn.Module):
-    """
-    Compute contrastive loss
-    """
-
-    def __init__(self, margin=0, measure=False, max_violation=False):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-        if measure == 'order':
-            self.sim = order_sim
-        else:
-            self.sim = cosine_sim
-
-        self.max_violation = max_violation
-
-    def forward(self, im, s):
-        # compute image-sentence score matrix
-        scores = self.sim(im, s)
-        diagonal = scores.diag().view(im.size(0), 1)
-        d1 = diagonal.expand_as(scores)
-        d2 = diagonal.t().expand_as(scores)
-
-        # compare every diagonal score to scores in its column
-        # caption retrieval
-        cost_s = (self.margin + scores - d1).clamp(min=0)
-        # compare every diagonal score to scores in its row
-        # image retrieval
-        cost_im = (self.margin + scores - d2).clamp(min=0)
-
-        # clear diagonals
-        mask = torch.eye(scores.size(0)) > .5
-        I = Variable(mask)
-        if torch.cuda.is_available():
-            I = I.cuda()
-        cost_s = cost_s.masked_fill_(I, 0)
-        cost_im = cost_im.masked_fill_(I, 0)
-
-        # keep the maximum violating negative for each query
-        if self.max_violation:
-            cost_s = cost_s.max(1)[0]
-            cost_im = cost_im.max(0)[0]
-
-        return cost_s.sum() + cost_im.sum()
-
-def cosine_sim(im, s):
-    """Cosine similarity between all the image and sentence pairs
-    """
-    return im.mm(s.t())
-
-
-def order_sim(im, s):
-    """Order embeddings similarity measure $max(0, s-im)$
-    """
-    YmX = (s.unsqueeze(1).expand(s.size(0), im.size(0), s.size(1))
-           - im.unsqueeze(0).expand(s.size(0), im.size(0), s.size(1)))
-    score = -YmX.clamp(min=0).pow(2).sum(2).sqrt().t()
-    return score
-
 
 @META_TASK.register()
-class TrainingStacMR(OpenEndedTask):
+class TrainingAoA(OpenEndedTask):
     def __init__(self, config):
         super().__init__(config)
         self.config = config
         self.tokenizer = get_tokenizer(config.DATASET.FEATURE_DATASET.TOKENIZER.PRETRAINED_NAME)
-        self.crit = LanguageModelCriterion()
-        self.criterion = ContrastiveLoss(margin=config.MODEL.LOSS_FN.MARGIN,
-                                         measure=config.MODEL.LOSS_FN.MEASURE,
-                                         max_violation=config.MODEL.LOSS_FN.MAX_VIOLATION)
 
-        self.crit.to(self.device)
-        self.criterion.to(self.device)
-        self.grad_clip = config.TRAINING.GRAD_CLIP
+
         #self.loss_fn = NLLLoss(ignore_index=self.vocab.padding_idx)
     
-    def adjust_learning_rate(self, optimizer, epoch):
-        """Sets the learning rate to the initial LR
-        decayed by 10 every 30 epochs"""
-        lr = self.config.TRAINING.LEARNING_RATE * (0.1 ** (epoch // self.config.TRAINING.LR_UPDATE))
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
     
     def create_dict_dataloaders(self, config):
         # creating dictionary iterable-dataset data loader
@@ -158,23 +89,17 @@ class TrainingStacMR(OpenEndedTask):
                 for it, items in enumerate(dataloader):
                     items = items.to(self.device)
                     with torch.no_grad():
-                        results = self.model(items, mode='inference')
-
-                    seq_prob = results['scores'].contiguous()
-                    img_emb = results['img_emb']
-                    cap_emb = results['cap_emb']
-                    # out = F.log_softmax(out, dim=-1)
+                        outs = self.model(items)
                     
                     shifted_right_answer_tokens = torch.roll(items.answer_tokens.squeeze(), shifts=1, dims=-1)
                     # loss = self.loss_fn(out.view(-1, out.shape[-1]), shifted_right_answer_tokens.view(-1))
                     
-                    answer_masks = items.answer_masks.squeeze()
+                    answer_masks = torch.roll(items.answer_masks.squeeze(), shifts=1, dims=-1)
                     
-                    caption_loss = self.crit(seq_prob, 
+                    caption_loss = self.crit(outs, 
                                              shifted_right_answer_tokens, 
                                              answer_masks)
                 
-                    retrieval_loss = self.criterion(img_emb, cap_emb)
                     
                     # loss = 2.0 * retrieval_loss + caption_loss
                     loss = caption_loss # Focus on text generation
@@ -197,8 +122,7 @@ class TrainingStacMR(OpenEndedTask):
             for it, items in enumerate(dataloader):
                 items = items.to(self.device)
                 with torch.no_grad():
-                    results = self.model(items)
-                outs = results["predicted_token"]
+                    outs = self.model(items)
 
                 answers_gt = items.answers
                 answers_gen = self.tokenizer.batch_decode(outs,
@@ -221,29 +145,21 @@ class TrainingStacMR(OpenEndedTask):
             for it, items in enumerate(self.train_dataloader):
                 # self.adjust_learning_rate(self.optim, self.epoch)
                 items = items.to(self.device)
-                results = self.model(items)
-                seq_probs = results["scores"].contiguous()
-                img_emb = results['img_emb']
-                cap_emb = results['cap_emb']
-                #out = F.log_softmax(out, dim=-1)
+                out = self.model(items)
 
                 shifted_right_answer_tokens = torch.roll(items.answer_tokens.squeeze(), shifts=1, dims=-1)
+                answer_masks = torch.roll(items.answer_masks.squeeze(), shifts=1, dims=-1)
                 self.optim.zero_grad()
                 # loss = self.loss_fn(out.view(-1, out.shape[-1]), shifted_right_answer_tokens.view(-1))
-                answer_masks = items.answer_masks.squeeze()
-                caption_loss = self.crit(seq_probs, 
+                
+                caption_loss = self.crit(out, 
                                          shifted_right_answer_tokens, 
                                          answer_masks)
-                
-                retrieval_loss = self.criterion(img_emb, cap_emb)
                 
                 # loss = 2.0 * retrieval_loss + caption_loss
                 loss = caption_loss
                 
                 loss.backward()
-
-                if self.grad_clip > 0:
-                    clip_grad_norm_(self.model.parameters(), self.grad_clip)
                     
                 self.optim.step()
                 this_loss = loss.item()
@@ -319,8 +235,7 @@ class TrainingStacMR(OpenEndedTask):
             for it, items in enumerate(self.test_dict_dataloader):
                 items = items.to(self.device)
                 with torch.no_grad():
-                    result = self.model(items)
-                outs = result["predicted_token"]
+                    outs = self.model(items)
 
                 answers_gt = items.answers
                 answers_gen = self.tokenizer.batch_decode(outs,
