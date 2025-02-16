7e9cfb376f4ee90c1e5a3bba8f6aa0e85efd66cb
DucAnh0Cap <danhngn1403@gmail.com>
2025-02-16 16:41:22 +0700

    Fix minor bug in ocr_vocab.py

--- data_utils/vocabs/ocr_vocab.py
+++ data_utils/vocabs/ocr_vocab.py
@@ -88,8 +88,9 @@ class OcrVocab(Vocab):
             (in the index address space, the OOV tokens are after the fixed vocab)
         '''
         indices = []
-        for word in text[0].split(' '):
-            
+        if isinstance(text, list):
+            text = text[0].split(' ')
+        for word in text:
             matched_inds = []
             # match answer to fixed vocab
             matched_inds.append(self.stoi[word])
