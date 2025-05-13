4bd561563d710692346d38b4b5b20a50fe9ca6cf
DucAnh0Cap <danhngn1403@gmail.com>
2025-05-13 21:38:06 +0700

    adjist stacmr config and is_vietnamese function

--- data_utils/utils.py
+++ data_utils/utils.py
@@ -276,7 +276,11 @@ def is_Vietnamese(word: str) -> tuple[bool, tuple]:
         "giêng": "giiêng", # giếng after being removed the tone
         "giêt": "giiêt",   # giết after being removed the tone
         "giêc": "giiêc",   # giếc (diếc) after being removed the tone
-        "gi": "gii"        # gì after removing the tone 
+        "gi": "gii"        # gì after removing the tone,
+        "trròn": "tròn",   # tròn after removing the tone,
+        "khhung": "khung", # khung after removing the tone,
+        "cđã": "đã"     # đã after removing the tone,
+        "luạt": "luật", # luật after removing the tone,
     }
 
     if word in special_words_to_words:
